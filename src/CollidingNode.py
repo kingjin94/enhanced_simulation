7eb6d01b006acf397c1088bcf6f45bd98aee9c30
Matthias Mayer <matthias.mayer@tum.de>
2019-07-03 15:19:49 +0000

    A more complex collision publisher that also notes which links collided

--- src/CollidingNode.py
+++ src/CollidingNode.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import rospy
 from gazebo_msgs.msg import ContactsState
-from std_msgs.msg import Bool
+from enhanced_sim.msg import CollisionState
 
 """
 Spec:
@@ -12,28 +12,38 @@ publish a boolean variable (per link) saying whether in collision
 class CollidingNode(object):
 	def __init__(self, collision_state_update_freq = 10):
 		self.update_rate = rospy.Rate(collision_state_update_freq)
-		self.pub = rospy.Publisher('/panda/bumper/colliding', Bool, queue_size=10)
-		self.last_collision_time = rospy.get_rostime() # Save last collision time for cooldown
+		self.pub = rospy.Publisher('/panda/bumper/colliding', CollisionState, queue_size=10)
 
 		# find all bumper topics and subscribe
 		topics = rospy.get_published_topics()
+		self.observedLinks = [] # List of all links under observance
+		self.last_collision_times = {} # Last collision time for each link
 		for name, msgType in topics:
 			if msgType == "gazebo_msgs/ContactsState" and "/panda/bumper/panda_" in name:
-				rospy.Subscriber(name, ContactsState, self.collision_listener)
+				rospy.Subscriber(name, ContactsState, 
+					callback=self.collision_listener, callback_args=name)
+				self.observedLinks.append(name)
+				self.last_collision_times[name] = rospy.get_rostime()
 
-	def collision_listener(self, msg):
+	def collision_listener(self, msg, link_name):
 		if any(msg.states): # atleast one valid collision point
-			if msg.header.stamp > self.last_collision_time:
-				self.last_collision_time = msg.header.stamp	
+			if msg.header.stamp > self.last_collision_times[link_name]:
+				self.last_collision_times[link_name] = msg.header.stamp	
 	
 	def start(self):
 		while not rospy.is_shutdown():
 			# Have less than 10 ms passed since last collision?
-			self.pub.publish(Bool(rospy.get_rostime() <= rospy.Duration(1, 10000000) + self.last_collision_time))
+			msg = CollisionState()
+			msg.colliding = False
+			for k, v in self.last_collision_times.items():
+				if rospy.get_rostime() <= rospy.Duration(1, 10000000) + v:
+					msg.colliding = True
+					msg.collidingLinks.append(k)
+			self.pub.publish(msg)
 			self.update_rate.sleep() # Wait till next update needs to be sent
 
 if __name__ == '__main__':
-	rospy.init_node("BoolCollGenerator", anonymous=True)
+	rospy.init_node("SimpleCollStateGenerator", anonymous=True)
 	thisNode = CollidingNode()
 	thisNode.start()
 
