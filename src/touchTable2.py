c42ebd0adb3611caa388b83b47283dc364746ea3
Matthias Mayer <matthias.mayer@tum.de>
2019-10-01 15:16:54 +0000

    Split touch table into refiner for anything and one for tables

--- src/touchTable2.py
+++ src/touchTable2.py
@@ -26,11 +26,8 @@ import tf
 from enhanced_sim.msg import CollisionState
 from gazebo_msgs.msg import ContactsState
 
-class tableRefiner:
+class TactileRefiner(object):
 	def __init__(self,  group_name = "panda_arm" ):
-		# Publisher for refined table
-		self.table_publisher = rospy.Publisher("/octomap_new/table_touched", table, queue_size=1, latch=True)
-		
 		# Moveit init
 		moveit_commander.roscpp_initialize(sys.argv)
 		self.robot = moveit_commander.RobotCommander()
@@ -47,13 +44,6 @@ class tableRefiner:
 		
 		# Start working
 		self.go_home()
-		
-		print("Waiting for table message ...")
-		self.table_msg = rospy.wait_for_message("/octomap_new/table", table)
-		
-		self.touched_points = []
-		
-		self.max_reach = 1.0 # Maximum distance from 0,0,0 the robot might be able to reach
 	
 	def wait_for_joint_arrival(self, q):
 		new = True
@@ -82,44 +72,28 @@ class tableRefiner:
 		client.send_goal_and_wait(goal)
 		if wait:
 			self.wait_for_joint_arrival(q)
-		
+	
 	def go_home(self):
 		print("Homing ...")
 		self.go_to_q(np.asarray([0.0, -1.5, 0.0, -1.8702679826187074, 0, 2.873639813867795 - 1.51, 0.06015034910227879])) # home configuration
-		
-	def sample_pose_over_table(self, ox = 1, oy = 0, oz = 0, ow = 0):
-		pose_goal = geometry_msgs.msg.Pose()
-		pose_goal.position.z = self.table_msg.centroid_position.z + 0.3
-		pose_goal.orientation.x = ox # tool pointing down
-		pose_goal.orientation.y = oy
-		pose_goal.orientation.z = oz
-		pose_goal.orientation.w = ow
-		
-		# sample postions over table till one is reachable
-		while not rospy.is_shutdown():
-			pose_goal.position.x = random.uniform(self.table_msg.min.x+.1, self.table_msg.max.x-.1)
-			pose_goal.position.y = random.uniform(self.table_msg.min.y+.1, self.table_msg.max.y-.1)
-			
-			if pose_goal.position.x**2+pose_goal.position.y**2+pose_goal.position.z**2 < self.max_reach**2:
-				return pose_goal
-		
+	
 	def get_current_pose(self):	
 		return self.move_group.get_current_pose().pose
-			
+	
 	def position_msg_to_numpy(self, position_msg):
 		return np.asarray((position_msg.x, position_msg.y, position_msg.z))
-		
+	
 	def orientation_msg_to_numpy(self, orientation_msg):
 		return np.asarray((orientation_msg.x, orientation_msg.y, \
 			orientation_msg.z, orientation_msg.w))
-			
+	
 	def numpy_to_position(self, arr):
 		pos = geometry_msgs.msg.Point()
 		pos.x = arr[0]
 		pos.y = arr[1]
 		pos.z = arr[2]
 		return pos
-		
+	
 	def numpy_to_orientation(self, arr):
 		ori = geometry_msgs.msg.Quaternion()
 		ori.x = arr[0]
@@ -127,23 +101,23 @@ class tableRefiner:
 		ori.z = arr[2]
 		ori.w = arr[3]
 		return ori
-		
+	
 	def get_current_position(self):
 		return self.position_msg_to_numpy(self.get_current_pose().position)
-		
+	
 	def get_current_rotation(self):
 		return self.orientation_msg_to_numpy(self.get_current_pose().orientation)
-		
+	
 	def get_distance_to_position(self, pos):
 		if type(pos) == geometry_msgs.msg.Point:
 			pos = self.position_msg_to_numpy(pos)
 		return np.linalg.norm(self.get_current_position() - pos)
-		
+	
 	def get_distance_to_rot(self, rot):
 		if type(rot) == geometry_msgs.msg.Quaternion:
 			rot = self.orientation_msg_to_numpy(rot)
 		return np.linalg.norm(self.get_current_rotation() - rot)
-
+	
 	def go_to_pose(self, pose):
 		(plan, fraction) = self.move_group.compute_cartesian_path([pose], 0.01, 1.0)
 		exec_time = plan.joint_trajectory.points[-1].time_from_start.to_sec()
@@ -164,7 +138,53 @@ class tableRefiner:
 		except Exception as e:
 			print(e)
 			return False
+	
+	def p_step(self, pos_d, ori_d):
+			""" Make a step proportional to an error """
+			pos_e = pos_d - self.get_current_position()
+			rot_e = ori_d - self.get_current_rotation()
 			
+			rot_e = np.asarray(tf.transformations.euler_from_quaternion(rot_e)) / 300
+			e = np.concatenate([pos_e, rot_e])
+			print("Error: {}".format(e))
+			
+			q = np.asarray(self.robot.get_current_state().joint_state.position[:7])
+			J = np.asarray(self.kdl_kin.jacobian(q))
+			
+			q_new = q+J.T.dot(e)
+			self.go_to_q(q_new, wait=False)
+	
+
+class TableRefiner(TactileRefiner):
+	def __init__(self, group_name = "panda_arm"):
+		super(TableRefiner, self).__init__(group_name)
+		
+		# Publisher for refined table
+		self.table_publisher = rospy.Publisher("/octomap_new/table_touched", table, queue_size=1, latch=True)
+		
+		print("Waiting for table message ...")
+		self.table_msg = rospy.wait_for_message("/octomap_new/table", table)
+		
+		self.touched_points = []
+		
+		self.max_reach = 1.0 # Maximum distance from 0,0,0 the robot might be able to reach
+	
+	def sample_pose_over_table(self, ox = 1, oy = 0, oz = 0, ow = 0):
+		pose_goal = geometry_msgs.msg.Pose()
+		pose_goal.position.z = self.table_msg.centroid_position.z + 0.3
+		pose_goal.orientation.x = ox # tool pointing down
+		pose_goal.orientation.y = oy
+		pose_goal.orientation.z = oz
+		pose_goal.orientation.w = ow
+		
+		# sample postions over table till one is reachable
+		while not rospy.is_shutdown():
+			pose_goal.position.x = random.uniform(self.table_msg.min.x+.1, self.table_msg.max.x-.1)
+			pose_goal.position.y = random.uniform(self.table_msg.min.y+.1, self.table_msg.max.y-.1)
+			
+			if pose_goal.position.x**2+pose_goal.position.y**2+pose_goal.position.z**2 < self.max_reach**2:
+				return pose_goal
+		
 	def rotate_quart_around_z(self, quart, yaw=0.1):
 		turn = tf.transformations.quaternion_about_axis(yaw, (0,0,1))
 		quart = self.orientation_msg_to_numpy(quart)
@@ -176,11 +196,7 @@ class tableRefiner:
 		
 		return (self.table_msg.min.x+0.05 < pos[0] < self.table_msg.max.x-0.05) \
 			and (self.table_msg.min.y+0.05 < pos[1] < self.table_msg.max.y-0.05)
-			
-	def ori_geq_zero(self, a):
-
-		return a
-	
+		
 	def go_to_random_point_over_table(self, call_count=0):
 		timeout = 100
 		goal_pose = self.sample_pose_over_table()
@@ -202,21 +218,6 @@ class tableRefiner:
 				return True
 				
 		return False
-		
-	def p_step(self, pos_d, ori_d):
-		""" Make a step proportional to an error """
-		pos_e = pos_d - self.get_current_position()
-		rot_e = ori_d - self.get_current_rotation()
-		
-		rot_e = np.asarray(tf.transformations.euler_from_quaternion(rot_e)) / 300
-		e = np.concatenate([pos_e, rot_e])
-		print("Error: {}".format(e))
-		
-		q = np.asarray(self.robot.get_current_state().joint_state.position[:7])
-		J = np.asarray(self.kdl_kin.jacobian(q))
-		
-		q_new = q+J.T.dot(e)
-		self.go_to_q(q_new, wait=False)
 		
 	def approach_table_straight(self):
 		""" Approaches the table by moving along the long axis of the probing tool till a collision is sensed """
@@ -273,6 +274,8 @@ class tableRefiner:
 
 		n = np.asarray((xy*yz - xz*yy,xy*xz - yz*xx,det_z))
 		n = n/np.linalg.norm(n)
+		if(n[2] < 0):
+			n = -1.*n
 		print("Normal of touch surface: {}".format(n))
 		
 		new_table = deepcopy(self.table_msg)
@@ -301,7 +304,7 @@ class tableRefiner:
 
 if __name__ == '__main__':
 	rospy.init_node('touch_table', anonymous=True)
-	refiner = tableRefiner()
+	refiner = TableRefiner()
 
 	try:
 		refiner.touch_and_refine_table(touchPts=2)
